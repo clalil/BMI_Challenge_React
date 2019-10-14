c7fa274c5ffbf3baa922fba042c418f611169de2
clalil <cliljander@gmail.com>
2019-10-14 13:40:22 +0200

    Refactor AppInfo component.

--- src/Components/AppInfo.js
+++ src/Components/AppInfo.js
@@ -11,6 +11,19 @@ const AppInfo = () => {
     BMI can also give misleading results if you are very tall. 
     For children, adolescents and older people other recommendations apply.
     </p>
+    <ul>
+        {[["<18.5", "Underweight"],["18.5 < 25", "Normal"],["25 < 30", "Overweight"],[">30", "Obese"]].map(
+          arr => {
+            return (
+            <li>
+              <span style={{ fontWeight: "700" }}>A BMI of {arr[0]}</span>
+              <span style={{ padding: "0.4rem" }}> means you're  </span>
+              <span style={{ fontWeight: "700" }}>{arr[1]}</span>
+            </li>
+            )
+          }
+        )}
+      </ul>
     </>
   )
 }
