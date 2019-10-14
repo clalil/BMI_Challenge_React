3c02ed5106845b1a1bb65b3fb3bb8439c5da14da
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-14 11:38:51 +0200

    Add function that returns BMI message to BMIcalculator module & import that in displayResult component

--- src/Modules/BMICalculator.js
+++ src/Modules/BMICalculator.js
@@ -16,3 +16,18 @@ export const bmiCalculation = (weight, height, method) => {
     return `You are ${BMIMessage} with a BMI of ${finalBMI}`
   }
 }
+
+const setBMIMessage = (finalBMI) => {
+  if (finalBMI < 18.5) {
+    return 'Underweight'
+  }
+  if (finalBMI > 18.5 && finalBMI < 25) {
+    return "Normal"
+  }
+  if (finalBMI > 25 && finalBMI < 30) {
+    return "Overweight"
+  }
+  if (finalBMI > 30) {
+    return "Obese"
+  }
+}
