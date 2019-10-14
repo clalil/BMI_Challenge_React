998e67f89488870d8bd19753e958a5bdcb4601ce
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-14 17:11:48 +0200

    Add test for calculating BMI
    Switch from npm to yarn.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import AppInfo from './Components/AppInfo'
 import Calculator from "./Components/Calculator"
 import Footer from "./Components/Footer"
